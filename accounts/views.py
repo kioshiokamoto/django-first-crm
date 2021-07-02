906b27175806d6ac2be154208f1497883e7528ce
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-01 23:24:58 -0500

    :dart: Improve: agregar app accounts y rutas

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Home page')
+
+
+def products(request):
+    return HttpResponse('Products page')
+
+
+def customer(request):
+    return HttpResponse('Customer page')
