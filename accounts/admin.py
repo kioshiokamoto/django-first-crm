608cdfad905aba99dd63271c8d69e74d2ffe0ca9
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-04 18:48:43 -0500

    :dart: Improve: crear modelos y migraciones

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
