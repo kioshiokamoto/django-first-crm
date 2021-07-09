f284215b9325340df90d009f7860c982a1f841f6
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-08 23:07:43 -0500

    :dart: Improve: agregar relaciones entre tablas

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,3 +8,4 @@ from .models import *
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Product)
+admin.site.register(Tag)
--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -24,6 +31,10 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -32,7 +43,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # customer =
-    # product =
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
