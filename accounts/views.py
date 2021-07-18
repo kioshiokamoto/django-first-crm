43ecde227a44caef93b8ae3740f1dc66e5fc9b5b
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-18 18:05:25 -0500

    :dart: Improve: agregar ruta dinamica

--- accounts/views.py
+++ accounts/views.py
@@ -24,5 +24,13 @@ def products(request):
     return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    context = {'customer': customer,
+               "orders": orders, "order_count": order_count}
+
+    return render(request, 'accounts/customer.html', context)
