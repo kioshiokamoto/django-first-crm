f38f10c2cf4ce598d94bffdab23fe8765c063ffe
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-20 22:11:52 -0500

    :dart: Improve: agregar envio de correo electronico y reset de password

--- crm/settings.py
+++ crm/settings.py
@@ -136,3 +136,12 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# SMTP CONFIG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kjor29@gmail.com'
+EMAIL_HOST_PASSWORD = ''
